1addf03245e45560adaf57124d621aa26a1527b5
lixinghanofchina-hue <lixinghanofchina@gmail.com>
2025-09-03 11:29:48 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
+    double weight=130;
+    double height=61;
+    cout <<"Transfer pounds and inches to kilograms and meters"<<endl;
+    double weightKg=static_cast<double>(130*0.453592); 
+    double heightM=static_cast<double>(61*0.0254);
+    double BMI=weightKg/(heightM*heightM);
+    cout<<"The BMI for this person is : "<<BMI<<endl;
  
 
 
